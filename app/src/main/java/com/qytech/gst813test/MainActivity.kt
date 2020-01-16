85b1bdb4c9fdc548cf9a44316f6c3ff1509bd298
Jax <fuliby2loveyou@gmail.com>
2020-01-16 17:43:08 +0800

    add read and write file permission

--- app/src/main/java/com/qytech/gst813test/MainActivity.kt
+++ app/src/main/java/com/qytech/gst813test/MainActivity.kt
@@ -1,18 +1,61 @@
 package com.qytech.gst813test
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.qytech.gst813test.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val REQUEST_CODE_PERMISSIONS = 0x01
+    }
+
+    private val permissions = arrayOf(
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        if (!checkPermissions()) {
+            requestPermission()
+        }
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
+        }
+    }
+
+    private fun checkPermissions(): Boolean {
+        return permissions.all {
+            ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE_PERMISSIONS)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        grantResults.any {
+            it == PackageManager.PERMISSION_DENIED
+        }.let { needRequest ->
+            if (needRequest) {
+                requestPermission()
+            }
         }
     }
 
